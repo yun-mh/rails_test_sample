5df498753cc49ac4d05baf20a5f76991a0b632cd
Minho Yun <minhoyun.dev@gmail.com>
2022-06-19 19:15:39 +0900

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,15 @@
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  # parallelize(workers: :number_of_processors)
+  # Can disable parallelizing like below
+  parallelize(workers: 1)
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
